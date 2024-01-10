ee0796a3d9282620c3af379a554bd55ee547d038
Rafal <rav.kuklinski@gmail.com>
2024-01-10 11:41:16 +0100

    Server running on EV3 and handling commands

--- command_parser.py
+++ command_parser.py
@@ -10,7 +10,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-
+#This is a v1 of server
 
 # Create your objects here.
 ev3 = EV3Brick()
